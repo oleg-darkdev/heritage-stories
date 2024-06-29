9a27d191482be0f552ef5500fc15baee0958a84c
DarkDev <darkdev_brand@riseup.net>
2024-06-29 03:56:03 +0300

    refactoring links logic

--- src/lib/widgets/ui/landing/Nav.svelte
+++ src/lib/widgets/ui/landing/Nav.svelte
@@ -1,10 +1,31 @@
-<div class="nav_wrapper">
+<script>
+	const links = [
+		{
+			title: 'Lorem Ipsum ',
+			href: '/'
+		},
+		{
+			title: 'Lorem Ipsum ',
+			href: '/'
+		},
+		{
+			title: 'Lorem Ipsum ',
+			href: '/'
+		},
+		{
+			title: 'Lorem Ipsum ',
+			href: '/'
+		},
+		{
+			title: 'Lorem Ipsum ',
+			href: '/'
+		},
+
+	]
+</script>
+
+<section class="nav_wrapper">
 	<div
-		data-collapse="medium"
-		data-animation="over-right"
-		data-duration="400"
-		data-easing="ease-out-quint"
-		data-easing2="ease-out-quint"
 		role="banner"
 		class="nav_component w-nav"
 	>
@@ -13,16 +34,15 @@
 				<div class="banner5_component is-feet">
 					<div class="banner5_content">
 						<div class="text-color-white">
-							Ik ben tot 6 december afwezig i.v.m. zwangerschapsverlof
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry.
 						</div>
 					</div>
 					<a
-						data-w-id="6d170e25-c037-15ed-07f4-fb66d4e28420"
-						href="#"
+						href="/"
 						class="banner5_close-button w-inline-block"
 					>
 						<img
-							src="images/6299b8b0e1db1637aa896662_icon_close-banner.svg"
+							src="images/icon_close-banner.svg"
 							loading="lazy"
 							alt=""
 							class="invert"
@@ -32,23 +52,20 @@
 			</div>
 		</section>
 		<div class="nav_container">
-			<a href="/" aria-current="page" class="nav_logo-link w-inline-block w--current">
+			<a href="/" class="nav_logo-link w-inline-block w--current">
 				<img
-					src="images/6186e7f0f9e6f55e237a810c_feet-solution-logo.png"
+					src="images/logos/heritage-stories.svg"
 					loading="lazy"
 					sizes="(max-width: 991px) 100px, 120px"
-					srcset="images/6186e7f0f9e6f55e237a810c_feet-solution-logo-p-500.png 500w, images/6186e7f0f9e6f55e237a810c_feet-solution-logo-p-800.png 800w, images/6186e7f0f9e6f55e237a810c_feet-solution-logo-p-1080.png 1080w, images/6186e7f0f9e6f55e237a810c_feet-solution-logo-p-1600.png 1600w, images/6186e7f0f9e6f55e237a810c_feet-solution-logo-p-2000.png 2000w, images/6186e7f0f9e6f55e237a810c_feet-solution-logo-p-2600.png 2600w, images/6186e7f0f9e6f55e237a810c_feet-solution-logo-p-3200.png 3200w, images/6186e7f0f9e6f55e237a810c_feet-solution-logo.png 4589w"
-					alt="Feet Solution Logo"
+					alt="THE Heritage Stories Logo"
 					class="nav_logo"
 				/>
 			</a>
 			<nav role="navigation" class="nav_menu w-nav-menu">
 				<div class="div-block-4">
-					<a href="#about" class="nav_link w-nav-link">Praktijk</a>
-					<a href="#services" class="nav_link w-nav-link">behandelingen</a>
-					<a href="#products" class="nav_link w-nav-link">Producten</a>
-					<a href="#slider" class="nav_link w-nav-link">Media</a>
-					<a href="#contact" class="nav_link w-nav-link">Contact</a>
+					{#each links as link}
+						<a href="{link.href}" class="nav_link w-nav-link">{link.title}</a>
+					{/each}
 				</div>
 			</nav>
 			<div class="nav_menu-button w-nav-button">
@@ -61,4 +78,4 @@
 		</div>
 	</div>
 	<div class="nav_blank-space" />
-</div>
+</section>
