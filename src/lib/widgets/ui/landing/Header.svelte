8315400dd4dddea744a5e67c5e487c6c6d318d34
DarkDev <darkdev_brand@riseup.net>
2024-06-29 03:56:58 +0300

    upd content

--- src/lib/widgets/ui/landing/Header.svelte
+++ src/lib/widgets/ui/landing/Header.svelte
@@ -1,24 +1,27 @@
-<header id="header" class="section-header">
+<header class="section-header">
     <div class="page-padding">
     <div class="container-large">
         <div class="padding-vertical padding-huge">
         <div class="header_component">
             <div class="margin-bottom margin-large">
-            <h1 class="max-width-large">De <span class="text-style-link">juiste</span> zorg voor uw voeten </h1>
+            <h1 class="max-width-large">Lorem <span class="text-style-link">Ipsum</span> is simply dummy </h1>
             </div>
             <div class="margin-bottom margin-large">
-            <p class="text-size-medium max-width-medium">Pedicuresalon Feet Solution helpt u naar gezonde voeten. Met mijn subtiele zorg, huiselijke sfeer en vakkundige werkwijze, bied ik de beste zorg die past bij uw voeten. <br>U hoeft nooit meer uw voeten in het zand te steken. <br>‍ <strong>Oók beschikbaar in de avonden en op zaterdagen.</strong>
-            </p>
+            <p class="text-size-medium max-width-medium">
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.            </p>
+            <p/>            
             </div>
             <div class="margin-bottom margin-medium">
-            <a href="#sz-booking-toggle" class="w-inline-block">
-                <p class="text-style-handwritten max-width-medium">Kom eens langs</p>
+            <a href="/" class="w-inline-block">
+                <p class="text-style-handwritten max-width-medium">is simply dummy</p>
             </a>
             </div>
             <div class="header_testomonial">
-            <img src="images/618e69275ac26a35012dcc78_emoji.png" loading="lazy" alt="" class="image-9">
-            <div class="text-size-medium max-width-medium">"Ik kon zelfs in de avonden na mijn werk en in het weekend terecht. Dit komt perfect uit met mijn full-time baan. Dankzij de goede zorg bij Feet Solution zijn mijn voeten weer stralend. Ik beveel deze salon zeker aan!" <br>
-                <br>- Jasmijn R.
+            <img src="" loading="lazy" alt="" class="image-9">
+            <div class="text-size-medium max-width-medium">
+                <p>
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.            </p>
+                <br>- Lorem Ipsum is simply
             </div>
             </div>
         </div>
