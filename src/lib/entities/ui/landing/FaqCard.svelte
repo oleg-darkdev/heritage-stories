79112ef1fd7dbf29828e280c268c4f77b524a5cb
DarkDev <darkdev_brand@riseup.net>
2024-06-29 03:58:10 +0300

    refacetroing cards

---
+++ src/lib/entities/ui/landing/FaqCard.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let faq;
+</script>
+
+
+<div  class="faq_item">
+	<div class="faq_question" on:click={()=> faq.showAnswer = !faq.showAnswer}>
+	<h5 class="heading-small">{faq.q}</h5>
+	<!-- <div  class="faq-arrow"></div> -->
+	</div>
+	{#if faq.showAnswer}
+		<div class="faq_answer">
+		<p class="faq_answer-text">
+			<strong>Uw voeten zijn letterlijk de basis van uw lichaam.</strong> Denk er maar eens over na hoeveel een mens loopt. Veel mensen vergeten hun voeten en gaan pas naar een pedicure wanneer ze last krijgen van bijvoorbeeld likdoorns of eeltplekken. Wanneer u regelmatig een pedicure bezoekt, kunnen deze grotendeels klachten voorkomen worden.
+		</p>
+		</div>
+	{/if}
+</div>
---
+++ src/lib/entities/ui/landing/ScenarionCard.svelte
@@ -0,0 +1,18 @@
+<script>
+
+  export let scenario;
+</script>
+
+<div class="service_item">
+	<div class="margin-bottom margin-small">
+		<h6 class="text-size-large">{scenario.title}</h6>
+	</div>
+	<div class="margin-bottom margin-small">
+		<div>
+      
+			<!--  of voor <strong>Loewn</strong> -->
+      {#each scenario.desc as desc}<p>{desc}</p>{/each}
+		</div>
+	</div>
+	<a href="/" class="button w-button">{scenario.country}</a>
+</div>
