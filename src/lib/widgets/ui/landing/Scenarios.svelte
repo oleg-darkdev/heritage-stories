94958b6d7e50a73f02cd9d08b5bd82fc934f4571
DarkDev <darkdev_brand@riseup.net>
2024-06-29 03:57:05 +0300

    refactoring cards

--- src/lib/widgets/ui/landing/Scenarios.svelte
+++ src/lib/widgets/ui/landing/Scenarios.svelte
@@ -1,89 +1,74 @@
+<script>
+  import {ScenarionCard} from '$entities';
+
+  const scenarios = [
+    {
+      id: 0,
+      title: `lorem Ipsum is simply dummy text`,
+      desc: [`lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.`, ` and scrambled it to make a type specimen book.`],
+      country: `lorem Ipsum `,
+      content: [
+        ``, ``, ``
+      ]
+    },
+    {
+      id: 0,
+      title: `lorem Ipsum is simply dummy text`,
+      desc: [`lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.`, ` and scrambled it to make a type specimen book.`],
+      country: `lorem Ipsum `,
+      content: [
+        ``, ``, ``
+      ]
+    },
+    {
+      id: 0,
+      title: `lorem Ipsum is simply dummy text`,
+      desc: [`lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.`, ` and scrambled it to make a type specimen book.`],
+      country: `lorem Ipsum `,
+      content: [
+        ``, ``, ``
+      ]
+    },
+    {
+      id: 0,
+      title: `lorem Ipsum is simply dummy text`,
+      desc: [`lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.`, ` and scrambled it to make a type specimen book.`],
+      country: `lorem Ipsum `,
+      content: [
+        ``, ``, ``
+      ]
+    },
+    {
+      id: 0,
+      title: `lorem Ipsum is simply dummy text`,
+      desc: [`lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.`, ` and scrambled it to make a type specimen book.`],
+      country: `lorem Ipsum `,
+      content: [
+        ``, ``, ``
+      ]
+    },
+    {
+      id: 0,
+      title: `lorem Ipsum is simply dummy text`,
+      desc: [`lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.`, ` and scrambled it to make a type specimen book.`],
+      country: `lorem Ipsum `,
+      content: [
+        ``, ``, ``
+      ]
+    },
+  ]
+</script>
+
+
 <section id="services" class="section-services">
     <div class="page-padding">
     <div class="container-large">
         <div class="padding-vertical padding-huge">
         <div class="services_component">
-            <div class="service_item">
-            <div class="margin-bottom margin-small">
-                <h6 class="text-size-large">Standaard Pedicure</h6>
-            </div>
-            <div class="margin-bottom margin-small">
-                <div>Voor de <strong>gewone voet</strong>, of voor <strong>voeten met wat problemen.</strong> Iedere voet is welkom. Tijdens deze behandeling worden uw voeten helemaal verzorgd van enkel tot teen. </div>
-            </div>
-            <a href="#sz-booking-toggle" class="button w-button">Bekijk alle prijzen</a>
-            </div>
-            <div class="service_item">
-            <div class="margin-bottom margin-small">
-                <h6 class="text-size-large">Uitgebreide behandeling</h6>
-            </div>
-            <div class="margin-bottom margin-small">
-                <div>Bent u nu echt toe aan wat ontspanning? Boek dan de uitgebreide behandeling, hierin ontvangt u <strong>de standaard behandeling</strong> maar dan worden uw voeten <strong>heerlijk gescrubd</strong>, krijgen ze <strong>een voetmasker</strong> en <strong>een heerlijke massage.</strong>
-                </div>
-            </div>
-            <a href="#sz-booking-toggle" class="button w-button">Bekijk alle prijzen</a>
-            </div>
-            <div class="service_item">
-            <div class="margin-bottom margin-small">
-                <h6 class="text-size-large">Deelbehandeling</h6>
-            </div>
-            <div class="margin-bottom margin-small">
-                <div>Heeft u nu alleen moeite met de nagels knippen, of ziet u liever alleen het eelt verdwijnen? Kies dan voor een deelbehandeling. <strong>Daarin wordt alleen hetgeen verholpen waar u last van heeft.</strong>
-                </div>
-            </div>
-            <a href="#sz-booking-toggle" class="button w-button">Bekijk alle prijzen</a>
-            </div>
-            <div class="service_item">
-            <div class="margin-bottom margin-small">
+          {#each scenarios as scenario}
+            <ScenarionCard {scenario} />
+          {/each}
 
-                <h6 class="text-size-large">Nagelreconstructie</h6>
-            </div>
-            <div class="margin-bottom margin-small">
-                <div>Nagelreconstructie is het <strong>herstellen en reconstrueren</strong> van beschadigde of misvormde nagels met behulp van <strong> gel.</strong> Verschillende nagelproblemen, waaronder kalknagels, schimmelnagels, ingegroeide nagels en stootnagels worden hiermee verholpen. </div>
-            </div>
-            <a href="#sz-booking-toggle" class="button w-button">Bekijk alle prijzen</a>
-            </div>
-            <div class="service_item">
-            <div class="margin-bottom margin-small">
-                <h6 class="text-size-large">Nagellak</h6>
-            </div>
-            <div class="margin-bottom margin-small">
-                <div>Ik werk met Orly breathable nagellak. <strong>Deze lak is zuurstofdoorlatend</strong>. Het is een 4 in 1 lak waardoor ik maar 1 lak nodig heb. Neem uw slippers mee. </div>
-            </div>
-            <a href="#sz-booking-toggle" class="button w-button">Bekijk alle prijzen</a>
-            </div>
-            <div class="service_item">
-            <div class="margin-bottom margin-small">
-                <h6 class="text-size-large">
-                <strong>Gellak</strong>
-                <br>
-                </h6>
-            </div>
-            <div class="margin-bottom margin-small">
-                <div>Kom eens kennismaken met <strong></strong>Gelish, ons award-winnende gellakmerk! <strong>Geniet van langer houdbare nagels</strong> zonder beschadiging met speciale pigmenten en uitstekende dekking in slechts 2 lagen. Probeer het nu uit en voel het verschil! </div>
-            </div>
-            <a href="#sz-booking-toggle" class="button w-button">Bekijk alle prijzen</a>
-            </div>
-            <div class="service_item">
-            <div class="margin-bottom margin-small">
-                <h6 class="text-size-large">Geen idee welke behandeling bij uw situatie past?</h6>
-            </div>
-            <div class="margin-bottom margin-small">
-                <div>Bel dan direct voor <strong>advies op maat, </strong>afgestemd op uw unieke situatie. Mijn missie is om uw klachten zo effectief mogelijk te behandelen. Daarbij deel ik graag mijn kennis zodat u de juiste zorg en aandacht krijgt. </div>
-            </div>
-            <a href="tel:+31630812905" class="button w-button">Direct bellen</a>
-            </div>
-            <div class="service_item">
-            <div class="margin-bottom margin-small">
-                <h6 class="text-size-large">
-                <strong>Afspraak op een andere dag</strong>
-                <br>
-                </h6>
-            </div>
-            <div class="margin-bottom margin-small">
-                <div>Liever afspreken op een dag <strong>buiten mijn openingstijden?</strong> Neem contact op en dan zullen we samen de mogelijkheden bespreken. </div>
-            </div>
-            <a href="tel:+31630812905" class="button w-button">Direct bellen</a>
-            </div>
         </div>
         </div>
     </div>
