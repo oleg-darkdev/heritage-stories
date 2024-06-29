b8b96cf326f1ce9870174189b28a4e3d6153bbd4
DarkDev <darkdev_brand@riseup.net>
2024-06-29 03:56:46 +0300

    refactoring faq cards

--- src/lib/widgets/ui/landing/Faq.svelte
+++ src/lib/widgets/ui/landing/Faq.svelte
@@ -1,92 +1,62 @@
-<section id="faq" class="section-faq">
+<script>
+    import {FaqCard} from '$entities';
+
+const faqData = [
+  {
+    q: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    a: [
+      `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+      `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.` 
+    ]
+  },
+  {
+    q: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    a: [
+      `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+      `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.` 
+    ]
+  },
+  {
+    q: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    a: [
+      `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+      `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.` 
+    ]
+  },
+  {
+    q: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    a: [
+      `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+      `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.` 
+    ]
+  },
+  {
+    q: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    a: [
+      `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+      `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.` 
+    ]
+  },
+]
+</script>
+
+<section id="faq"  class="section-faq">
           <div class="page-padding">
             <div class="container-large">
               <div class="padding-vertical padding-huge">
                 <div class="w-layout-grid faq_component">
                   <div class="faq_wrapper overflow-hidden">
                     <div class="margin-bottom margin-medium">
-                      <h2 class="faq_title">Veelgestelde <span class="text-style-link">vragen</span>
+                      <h2 class="faq_title">frequently asked <span class="text-style-link">questions.</span>
                       </h2>
                     </div>
-                    <div data-w-id="55ff560c-84ed-fc8b-95d9-004f0f49da21" class="faq_item">
-                      <div class="faq_question">
-                        <h5 class="heading-small">Waar is een pedicure goed voor?</h5>
-                        <div data-is-ix2-target="1" class="faq-arrow" data-w-id="55ff560c-84ed-fc8b-95d9-004f0f49da25" data-animation-type="lottie" data-src="https://assets-global.website-files.com/6186dd9dc65430f828674b53/6186dd9dc654304f4b674bb8_lottieflow-scroll-down-08-FF7455-easey.json" data-loop="0" data-direction="1" data-autoplay="0" data-renderer="svg" data-default-duration="1.5208333333333333" data-duration="0"></div>
-                      </div>
-                      <div style="height:0PX" class="faq_answer">
-                        <p class="faq_answer-text">
-                          <strong>Uw voeten zijn letterlijk de basis van uw lichaam.</strong> Denk er maar eens over na hoeveel een mens loopt. Veel mensen vergeten hun voeten en gaan pas naar een pedicure wanneer ze last krijgen van bijvoorbeeld likdoorns of eeltplekken. Wanneer u regelmatig een pedicure bezoekt, kunnen deze grotendeels klachten voorkomen worden.
-                        </p>
-                      </div>
-                    </div>
-                    <div data-w-id="55ff560c-84ed-fc8b-95d9-004f0f49da41" class="faq_item">
-                      <div class="faq_question">
-                        <h5 class="heading-small">Kom ik in aanmerking voor een pedicure?</h5>
-                        <div data-is-ix2-target="1" class="faq-arrow" data-w-id="55ff560c-84ed-fc8b-95d9-004f0f49da45" data-animation-type="lottie" data-src="https://assets-global.website-files.com/6186dd9dc65430f828674b53/6186dd9dc654304f4b674bb8_lottieflow-scroll-down-08-FF7455-easey.json" data-loop="1" data-direction="1" data-autoplay="0" data-renderer="svg" data-default-duration="1.5208333333333333" data-duration="0"></div>
-                      </div>
-                      <div style="height:0PX" class="faq_answer">
-                        <p class="faq_answer-text">
-                          <strong>Iedereen komt in aanmerking voor een pedicure.</strong> Overigens is het niet zo dat ik iedereen kan helpen. Ik ben geen medische pedicure. Heeft u dus onderliggende problemen zoals diabetes of reuma? Dan kan ik u helaas niet helpen. Twijfelt u? Neem dan contact met mij op, dan kijken we samen hoe en of ik u kan helpen.
-                        </p>
-                      </div>
-                    </div>
-                    <div data-w-id="55ff560c-84ed-fc8b-95d9-004f0f49da39" class="faq_item">
-                      <div class="faq_question">
-                        <h5 class="heading-small">Welke techniek(en) gebruikt Feet Solution?</h5>
-                        <div data-is-ix2-target="1" class="faq-arrow" data-w-id="55ff560c-84ed-fc8b-95d9-004f0f49da3d" data-animation-type="lottie" data-src="https://assets-global.website-files.com/6186dd9dc65430f828674b53/6186dd9dc654304f4b674bb8_lottieflow-scroll-down-08-FF7455-easey.json" data-loop="1" data-direction="1" data-autoplay="0" data-renderer="svg" data-default-duration="1.5208333333333333" data-duration="0"></div>
-                      </div>
-                      <div style="height:0PX" class="faq_answer">
-                        <p class="faq_answer-text">
-                          <strong>Ik werk met een motor met nattechniek.</strong> Dit betekend dat uit mijn handstuk een vloeistof komt. De vloeistof is vangend en heeft een verkoelende werking.
-                        </p>
-                      </div>
-                    </div>
-                    <div data-w-id="8f18598e-8530-8d35-7dc1-9477d62919ab" class="faq_item">
-                      <div class="faq_question">
-                        <h5 class="heading-small">Hoe lang duurt een afspraak maximaal?</h5>
-                        <div data-is-ix2-target="1" class="faq-arrow" data-w-id="8f18598e-8530-8d35-7dc1-9477d62919af" data-animation-type="lottie" data-src="https://assets-global.website-files.com/6186dd9dc65430f828674b53/6186dd9dc654304f4b674bb8_lottieflow-scroll-down-08-FF7455-easey.json" data-loop="1" data-direction="1" data-autoplay="0" data-renderer="svg" data-default-duration="1.5208333333333333" data-duration="0"></div>
-                      </div>
-                      <div style="height:0PX" class="faq_answer">
-                        <p class="faq_answer-text">
-                          <strong>Een afspraak duurt niet langer dan 60 minuten.</strong> Mocht u langer nodig hebben is dit in overleg. U betaalt per 15 minuten €7,50 extra.
-                        </p>
-                      </div>
-                    </div>
-                    <div data-w-id="68480bec-f6e7-cba0-5d7a-9558cdc42782" class="faq_item">
-                      <div class="faq_question">
-                        <h5 class="heading-small">Wat als ik te laat kom?</h5>
-                        <div data-is-ix2-target="1" class="faq-arrow" data-w-id="68480bec-f6e7-cba0-5d7a-9558cdc42786" data-animation-type="lottie" data-src="https://assets-global.website-files.com/6186dd9dc65430f828674b53/6186dd9dc654304f4b674bb8_lottieflow-scroll-down-08-FF7455-easey.json" data-loop="1" data-direction="1" data-autoplay="0" data-renderer="svg" data-default-duration="1.5208333333333333" data-duration="0"></div>
-                      </div>
-                      <div style="height:0PX" class="faq_answer">
-                        <p class="faq_answer-text">Indien u <strong>langer als 15 minuten te laat bent</strong>, kunt u behandeling niet meer doorgaan en kan de gehele behandeling in rekening gebracht worden. Houdt er rekening mee dat als u te laat komt, misschien niet de gehele behandeling uitgevoerd kan worden. </p>
-                      </div>
-                    </div>
-                    <div data-w-id="55ff560c-84ed-fc8b-95d9-004f0f49da29" class="faq_item">
-                      <div class="faq_question">
-                        <h5 class="heading-small">Hoe werken de betalingen?</h5>
-                        <div data-is-ix2-target="1" class="faq-arrow" data-w-id="55ff560c-84ed-fc8b-95d9-004f0f49da2d" data-animation-type="lottie" data-src="https://assets-global.website-files.com/6186dd9dc65430f828674b53/6186dd9dc654304f4b674bb8_lottieflow-scroll-down-08-FF7455-easey.json" data-loop="1" data-direction="1" data-autoplay="0" data-renderer="svg" data-default-duration="1.5208333333333333" data-duration="0"></div>
-                      </div>
-                      <div style="height:0PX" class="faq_answer">
-                        <p class="faq_answer-text">In de salon accepteer ik <strong>pin, contante betalingen en </strong>
-                          <a href="#sz-voucher-toggle">
-                            <strong>Feet Solution cadeaubonnen</strong>
-                          </a>
-                          <strong>.</strong> Een betaalverzoek is ook mogelijk.
-                        </p>
-                      </div>
-                    </div>
-                    <div data-w-id="55ff560c-84ed-fc8b-95d9-004f0f49da31" class="faq_item">
-                      <div class="faq_question">
-                        <h5 class="heading-small">Ik wil mijn afspraak graag verzetten, hoe doe ik dat?</h5>
-                        <div data-is-ix2-target="1" class="faq-arrow" data-w-id="55ff560c-84ed-fc8b-95d9-004f0f49da35" data-animation-type="lottie" data-src="https://assets-global.website-files.com/6186dd9dc65430f828674b53/6186dd9dc654304f4b674bb8_lottieflow-scroll-down-08-FF7455-easey.json" data-loop="1" data-direction="1" data-autoplay="0" data-renderer="svg" data-default-duration="1.5208333333333333" data-duration="0"></div>
-                      </div>
-                      <div style="height:0PX" class="faq_answer">
-                        <p class="faq_answer-text">Het beste kunt u even een bericht sturen naar <a href="tel:+31630812905" class="text-style-link">+31 6 30 81 29 05</a>, dan kunnen we <strong>samen kijken naar een datum</strong> wat voor u beter uitkomt. </p>
-                      </div>
-                    </div>
-                  </div>
-                  <div id="w-node-_55ff560c-84ed-fc8b-95d9-004f0f49da1c-82200de9" class="faq_visual">
-                    <img src="images/61880dc826f20840f4a5bc68_feet-unsplash.png" loading="lazy" sizes="(max-width: 991px) 100vw, 93vw" srcset="images/61880dc826f20840f4a5bc68_feet-unsplash-p-500.png 500w, images/61880dc826f20840f4a5bc68_feet-unsplash.png 755w" alt="" class="faq_image">
+                    {#each faqData as faq}
+                      <FaqCard {faq}/>
+                    {/each}
+                </div>
+
+                  <div  class="faq_visual">
+                    <img src="images/faqImg.svg" loading="lazy" sizes="(max-width: 991px) 100vw, 93vw" alt="F.A.Q. image" class="faq_image">
                   </div>
                 </div>
               </div>
