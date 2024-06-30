4738aa5e094c000025e2e4b68ba36bc9b5509f61
DarkDev <darkdev_brand@riseup.net>
2024-07-01 00:29:05 +0300

    intial app route

---
+++ src/routes/app/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	// import {  } from '$lib/shared';
+	// import { } from '$lib/widgets';
+  // import {  } from '$lib/entities'
+</script>
+
+
+<svelte:head>
+  <title>APP - Heritage Stories</title>
+</svelte:head>
+
+
+<style>
+</style>
