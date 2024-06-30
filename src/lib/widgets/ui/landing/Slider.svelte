b363bd1cb595566d33065529210ea780c415d285
DarkDev <darkdev_brand@riseup.net>
2024-07-01 00:28:07 +0300

    clear, add anchor

--- src/lib/widgets/ui/landing/Slider.svelte
+++ src/lib/widgets/ui/landing/Slider.svelte
@@ -28,13 +28,19 @@
   ]
 </script>
 
-<section id="slider" class="section-slider">
+<section class="section-slider">
+
+
           <section class="section-2 is--slider">
             <div class="slider__wave w-embed">
               <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 804 50.167" enable-background="new 0 0 804 50.167" xml:space="preserve">
                 <path fill="#FFECD9" d="M804,0v16.671c0,0-204.974,33.496-401.995,33.496C204.974,50.167,0,16.671,0,16.671V0H804z"></path>
               </svg>
             </div>
+
+            <a name='scenarios' /> 
+
+
             <div class="container-2 is--slider">
               <div class="splide">
                 <div class="splide__track w-dyn-list">
