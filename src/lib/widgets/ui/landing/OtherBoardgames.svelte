ebf52b079e9ba3035d9b00db26d4256d757e6bf7
DarkDev <darkdev_brand@riseup.net>
2024-06-30 23:15:16 +0300

    upd content

--- src/lib/widgets/ui/landing/OtherBoardgames.svelte
+++ src/lib/widgets/ui/landing/OtherBoardgames.svelte
@@ -1,30 +1,56 @@
 <script>
   const otherBoardgames = [
-    {
-      img: 'https://wallpaperset.com/w/full/c/9/0/522708.jpg',
-      title: 'Lorem Ipsum is simply dummy text',
-      link: '/',
-      desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`
-    },
-    {
-      img: 'https://wallpaperset.com/w/full/c/9/0/522708.jpg',
-      title: 'Lorem Ipsum is simply dummy text',
-      link: '/',
-      desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`
-    },
-    {
-      img: 'https://wallpaperset.com/w/full/c/9/0/522708.jpg',
-      title: 'Lorem Ipsum is simply dummy text',
-      link: '/',
-      desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`
-    },
-    {
-      img: 'https://wallpaperset.com/w/full/c/9/0/522708.jpg',
-      title: 'Lorem Ipsum is simply dummy text',
-      link: '/',
-      desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`
-    },
-  ]
+	{
+		title: 'PALEO-STORIES',
+		img: '/images/boardgames/paleo-stories.svg',
+		link: '/',
+		anchor: '#paleo-stories',
+		desc: `Paleo Stories - educational boardgame, enhanced by the interactive web application, that introduces players to the world of paleontological expeditions and the science of paleontology. `
+	},
+	{
+		title: 'ARCHAEO-STORIES',
+		img: '/images/boardgames/archaeo-stories.svg',
+		link: 'https://archaeo-stories.vercel.app/',
+		anchor: '#archaeo-stories',
+		desc: `Archaeo stories - educational boardgame, enhanced by an interactive web application, offers you to explore ancient grounds, unearth artifacts, and  become a real archaeologist! `
+	},
+	{
+		title: 'HERITAGE-STORIES',
+		img: '/images/boardgames/heritage-stories.svg',
+		link: '/',
+		anchor: '#heritage-stories',
+		desc: `Heritage stories - educational boardgame, enhanced by an interactive web application, about eco-friendly bicycle trip through the World Heritage Cities.`
+	},
+	{
+		title: 'CASTLE-STORIES',
+		img: '/images/boardgames/castle-stories.svg',
+		link: '/',
+		anchor: '#castle-stories',
+		desc: `Castle stories - educational boardgame, enhanced by an interactive web application, which will take you to the world of grand castles and ancient fortifications! Collect historical facts, complete tasks and unlock the secrets of ancient buildings to become a true connoisseur of medieval architecture.`
+	},
+	{
+		title: '12262-STORIES',
+		img: '/images/boardgames/12262-stories.svg',
+		link: '/',
+		anchor: '#12262-stories',
+		desc: `Explore the Kolskaya ultra-deep borehole, one of the deepest man-made facilities on the planet, drilled to a record depth of 12,262 meters. Discover a symbol of scientific and technical progress that has provided unique data for geological and geophysical research and generated many myths and legends and it's been called "the well to Hell."`
+  },	{
+		title: 'pyramid-stories',
+		img: '/images/boardgames/pyramid-stories.svg',
+		link: '/',
+		anchor: '#pyramid-stories',
+		desc: ``
+  },
+
+
+	{
+		title: 'Belarus/Poland-STORIES',
+		img: '/images/boardgames/by-pl-stories.svg',
+		link: 'https://by-pl.vercel.app/',
+		anchor: '#12262-stories',
+		desc: `"Cultures of Belarus and Poland" - through the materials prepared as part of this project, you will learn more about the relationship between these two neighboring countries, their history, culture and traditions.`	}
+];
+
 </script>
 
 <section id="products" class="section-products">
