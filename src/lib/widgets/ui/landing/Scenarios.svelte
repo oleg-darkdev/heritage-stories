de7f89e30166d92cd03c7f12f5271b46a71ede04
DarkDev <darkdev_brand@riseup.net>
2024-06-30 23:15:03 +0300

    upd content

--- src/lib/widgets/ui/landing/Scenarios.svelte
+++ src/lib/widgets/ui/landing/Scenarios.svelte
@@ -2,61 +2,20 @@
   import {ScenarionCard} from '$entities';
 
   const scenarios = [
-    {
-      id: 0,
-      title: `lorem Ipsum is simply dummy text`,
-      desc: [`lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.`, ` and scrambled it to make a type specimen book.`],
-      country: `lorem Ipsum `,
-      content: [
-        ``, ``, ``
-      ]
-    },
-    {
-      id: 0,
-      title: `lorem Ipsum is simply dummy text`,
-      desc: [`lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.`, ` and scrambled it to make a type specimen book.`],
-      country: `lorem Ipsum `,
-      content: [
-        ``, ``, ``
-      ]
-    },
-    {
-      id: 0,
-      title: `lorem Ipsum is simply dummy text`,
-      desc: [`lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.`, ` and scrambled it to make a type specimen book.`],
-      country: `lorem Ipsum `,
-      content: [
-        ``, ``, ``
-      ]
-    },
-    {
-      id: 0,
-      title: `lorem Ipsum is simply dummy text`,
-      desc: [`lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.`, ` and scrambled it to make a type specimen book.`],
-      country: `lorem Ipsum `,
-      content: [
-        ``, ``, ``
-      ]
-    },
-    {
-      id: 0,
-      title: `lorem Ipsum is simply dummy text`,
-      desc: [`lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.`, ` and scrambled it to make a type specimen book.`],
-      country: `lorem Ipsum `,
-      content: [
-        ``, ``, ``
-      ]
-    },
-    {
-      id: 0,
-      title: `lorem Ipsum is simply dummy text`,
-      desc: [`lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.`, ` and scrambled it to make a type specimen book.`],
-      country: `lorem Ipsum `,
-      content: [
-        ``, ``, ``
-      ]
-    },
-  ]
+	{
+		title: 'Education 4.0 goals',
+		desc: `The use of gamification and technology allows the educational process to be adapted to the individual needs of students and stimulates the development of core 21st century competencies such as critical thinking, creativity and collaboration, which is completely in accordance with the goals of Education 4.0.`
+	},
+	{
+		title: 'Eco-friendly materials',
+		desc: `The board games included in the stories are made of eco-friendly and 100% recyclable materials: wood, metal and eco-paper printed with eco-friendly ink.`
+	},
+	{
+		title: 'OPEN SOURCE',
+		desc: `The board game print materials and web application source code are free and publicly available in the repository for download and modification.`
+	}
+];
+  
 </script>
 
 
