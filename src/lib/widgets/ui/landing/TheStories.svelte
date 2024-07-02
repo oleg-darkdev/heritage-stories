9c93a22f851bbc9a1d93b14e6dacb41e2959dbb4
DarkDev <darkdev_brand@riseup.net>
2024-07-02 20:34:12 +0300

    upd content, upd margin

--- src/lib/widgets/ui/landing/TheStories.svelte
+++ src/lib/widgets/ui/landing/TheStories.svelte
@@ -1,20 +1,23 @@
-<section class="section-about">
+<section class="mb-20">
     <div class="page-padding">
     <div class="container-large">
         <div class="padding-vertical padding-huge">
         <div class="about_component">
             <div class="about_content">
             <div class="margin-bottom margin-medium">
-                <h2 class="heading-xlarge">Lorem impsum <br> lorem <span class="text-style-link">impsum</span>
+                <h2 class="heading-xlarge">
+                    Part of  <br>  <a href='https://the-stories.vercel.app/' target="_blank" class="text-style-link">THE Stories </a> educational project
                 </h2>
             </div>
             <div class="margin-bottom margin-xlarge">
                 <div class="text-size-medium">orem Ipsum is simply dummy text of the printing <strong>and typesetting industry</strong> Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.<strong>It has survived not only five centuries, but also the leap into ele.</strong>
                 </div>
             </div>
+            <img src="images/logos/heritage-stories.svg" loading="lazy" alt="Logo Heritage stories boardgame" class="logo-heritage">
+
             </div>
             <div class="about_visual">
-            <img src="images/theStoriesImg.svg" loading="lazy" sizes="(max-width: 479px) 100vw, (max-width: 767px) 93vw, (max-width: 991px) 640px, 46vw"  alt="THE Stories project image" class="about_illustration">
+            <img src="images/the-stories-banner.png" loading="lazy" sizes="(max-width: 479px) 100vw, (max-width: 767px) 93vw, (max-width: 991px) 640px, 46vw"  alt="THE Stories project image" class="about_illustration">
             </div>
         </div>
         </div>
