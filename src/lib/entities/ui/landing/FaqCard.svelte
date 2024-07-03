5c8ba303b9512578ee65742a8969b2f2bc7a77b2
DarkDev <darkdev_brand@riseup.net>
2024-07-03 23:15:43 +0300

    refactoring, add content

--- src/lib/entities/ui/landing/FaqCard.svelte
+++ src/lib/entities/ui/landing/FaqCard.svelte
@@ -3,16 +3,27 @@
 </script>
 
 
-<div  class="faq_item">
-	<div class="faq_question" on:click={()=> faq.showAnswer = !faq.showAnswer}>
+<div class="faq_item" on:click={()=> faq.showAnswer = !faq.showAnswer}>
+	<div class="faq_question" >
 	<h5 class="heading-small">{faq.q}</h5>
 	<!-- <div  class="faq-arrow"></div> -->
 	</div>
 	{#if faq.showAnswer}
 		<div class="faq_answer">
-		<p class="faq_answer-text">
-			<strong>Uw voeten zijn letterlijk de basis van uw lichaam.</strong> Denk er maar eens over na hoeveel een mens loopt. Veel mensen vergeten hun voeten en gaan pas naar een pedicure wanneer ze last krijgen van bijvoorbeeld likdoorns of eeltplekken. Wanneer u regelmatig een pedicure bezoekt, kunnen deze grotendeels klachten voorkomen worden.
-		</p>
+      {#each faq.a as answer}
+      <p class="faq_answer-text">
+        <!-- <strong>Lorem impsum</strong> -->
+        {answer}
+      </p>
+      {/each}
 		</div>
 	{/if}
 </div>
+
+
+<style>
+  .faq_answer-text{
+      margin-bottom:30px
+  }
+
+</style>
--- src/lib/widgets/ui/landing/Faq.svelte
+++ src/lib/widgets/ui/landing/Faq.svelte
@@ -1,68 +1,37 @@
 <script>
     import {FaqCard} from '$entities';
-
-const faqData = [
-  {
-    q: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    a: [
-      `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-      `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.` 
-    ]
-  },
-  {
-    q: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    a: [
-      `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-      `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.` 
-    ]
-  },
-  {
-    q: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    a: [
-      `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-      `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.` 
-    ]
-  },
-  {
-    q: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    a: [
-      `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-      `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.` 
-    ]
-  },
-  {
-    q: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    a: [
-      `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-      `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.` 
-    ]
-  },
-]
+    import {faq} from '$shared';
 </script>
 
 <section class="section-faq">
-          <div class="page-padding">
-            <div class="container-large">
-              <div class="padding-vertical padding-huge">
-                <div class="w-layout-grid faq_component">
-                  <div class="faq_wrapper overflow-hidden">
-                    <div class="margin-bottom margin-medium">
-                      <h2 class="faq_title">frequently asked <span class="text-style-link">questions.</span>
-                      </h2>
+  <div class="page-padding">
+    <div class="container-large">
+      <div class="padding-vertical padding-huge">
+        <div class="w-layout-grid faq_component">
+          <div class="faq_wrapper overflow-hidden">
+            <div class="margin-bottom margin-medium">
+              <h2 class="faq_title">Frequently Asked <span class="text-style-link text-[#ca8b0c]">Questions.</span>
+              </h2>
 
-                      <a name='faq' /> 
+              <a name='faq' />
 
-                    </div>
-                    {#each faqData as faq}
-                      <FaqCard {faq}/>
-                    {/each}
-                </div>
-
-                  <div  class="faq_visual">
-                    <img src="images/faqImg.svg" loading="lazy" sizes="(max-width: 991px) 100vw, 93vw" alt="F.A.Q. image" class="faq_image">
-                  </div>
-                </div>
-              </div>
             </div>
+            {#each faq as faq}
+              <FaqCard {faq}/>
+            {/each}
+        </div>
+
+          <div  class="faq_visual">
+            <img src="images/faqImg.svg" loading="lazy" sizes="(max-width: 991px) 100vw, 93vw" alt="F.A.Q. image" class="faq_image">
           </div>
-        </section>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<style>
+
+
+</style>
