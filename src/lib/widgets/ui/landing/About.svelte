6b29a2d942c8c13da8c876bbcd75bdfbe62bee5d
DarkDev <darkdev_brand@riseup.net>
2024-07-03 23:59:23 +0300

    upd text, add btn-link

--- src/lib/widgets/ui/landing/About.svelte
+++ src/lib/widgets/ui/landing/About.svelte
@@ -1,51 +1,41 @@
-<section  class="section-about">
-    <div class="page-padding">
+<section class="section-about">
+  <div class="page-padding">
     <div class="container-large">
-        <div class="padding-vertical padding-huge">
+      <div class="padding-vertical padding-huge">
         <div class="about_component">
-            <div class="about_visual">
+          <div class="about_visual">
             <img src="images/about.png" loading="lazy" alt="" class="about_illustration">
-            </div>
-            <div class="about_content">
+          </div>
+          <div class="about_content">
             <div class="margin-bottom margin-medium">
-                <div class="text-style-quote">
+              <div class="text-style-quote">
                 <span class="text-size-large is-quote">
-                    <!-- a href -->
-                    Oleg Medvedev
+                  <a href='https://oleg-darkdev.vercel.app/' class='text-style-link text-[#713f13]' target="_blank" > <strong>Oleg Medvedev </strong></a>
                 </span>
                 <br>
-
-                <a name='contact' /> 
+                <a name='contact' />
                 <span class="text-size-medium">Author & creator</span>
-                </div>
+              </div>
             </div>
             <div class="margin-bottom margin-xlarge">
-                <div class="text-size-medium">
-
-                    <p class='mb-2'>                      
-                        Product Web Engineer-IT Enthusiast, Gamification Coach, and JavaScript Programmer with  across various technologies,
-                      <strong> 4 years of experience in board game development and 3 years in web application development </strong>  including startups and open-source projects. See my open sorce projects are available on <strong><a class='text-style-link text-[#713f13]' href='https://github.com/oleg-darkdev' target="_blank">github profile.</a></strong>
-                    </p>
-                    <p class='mb-2'>
-                       Engaged in merging the digital and physical worlds to create engaging and educational experiences. Сollaborated with several 
-                      <strong> NGOs and museums in Belarus and Poland </strong>   on various historical projects. 
-                    </p>
-                    <p class='mb-2'>
-                        See my other historical projects and educational board games that <strong>combine history and modern technologies in my <a class='text-style-link text-[#713f13]' href='href="https://oleg-darkdev.vercel.app/' target="_blank"> portfolio.</a></strong>
-                    </p>              
-                    <p class='mb-2'>
-                      In head with a love for technologies, in heart with a love for people 💛 
-                    </p>  
-                  </div>
+              <div class="text-size-medium">
+                <p class='mb-2'> Product Web Engineer-IT Enthusiast, Gamification Coach, and JavaScript Programmer with across various technologies, <strong> 4 years of experience in board game development and 3 years in web application development </strong> including startups and open-source projects. See my open sorce projects are available on <strong>
+                    <a class='text-style-link text-[#713f13]' href='https://github.com/oleg-darkdev' target="_blank">github profile.</a>
+                  </strong>
+                </p>
+                <p class='mb-2'> Engaged in merging the digital and physical worlds to create engaging and educational experiences. Сollaborated with several <strong> NGOs and museums in Belarus and Poland </strong> on various historical projects. </p>
+                <p class='mb-2'> See my other historical projects and <strong> educational board games that combine history and modern technologies in my <a class='text-style-link text-[#713f13]' href='https://oleg-darkdev.vercel.app/' target="_blank"> portfolio.</a>
+                  </strong>
+                </p>
+                <p class='mb-2'>If you have ideas for collaboration - <strong>say me Hello from <a class='text-style-link text-[#713f13]' href='https://www.linkedin.com/in/oleg-darkdev' target="_blank">linkedin.</a></strong>
+                <br />
+                  In head with a love for technologies, in heart with a love for people 💛 </p>
+              </div>
             </div>
-            
-            </div>
-
+            <a href='https://oleg-darkdev.vercel.app/' target="_blank" class="button w-button">Get in touch</a>
+          </div>
         </div>
-
-        </div>
+      </div>
     </div>
-    </div>
+  </div>
 </section>
-
-        
